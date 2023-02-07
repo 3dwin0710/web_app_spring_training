c962e892a969d73a611186c4e5bbd3036eb218ad
Edwin LI <eli@et.esiea.fr>
2023-02-07 11:44:34 +0100

    Part 4 : Filter to add an HTTP header

--- src/main/java/fr/lernejo/todo/ApplicationIdentifierFilter.java
+++ src/main/java/fr/lernejo/todo/ApplicationIdentifierFilter.java
@@ -1,2 +1,26 @@
-package fr.lernejo.todo;public class ApplicationIdentifierFilter {
+package fr.lernejo.todo;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ApplicationIdentifierFilter implements Filter {
+
+
+    private final UUID uuid = UUID.randomUUID();
+    String Conversionuuid = uuid.toString();
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ApplicationIdentifierFilter newuuid = new ApplicationIdentifierFilter();
+        if(servletResponse instanceof HttpServletResponse){
+            ((HttpServletResponse) servletResponse).setHeader("Instance-ID",Conversionuuid);
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
 }
